99ff344f7b8ed049eb18fc7a1354899400cf5e97
Francisco José Plácido da Cunha <fj.placido@gmail.com>
2024-10-25 22:21:13 -0300

    Criando as principais entidades

--- src/main/java/entities/PlanoEstudo.java
+++ src/main/java/entities/PlanoEstudo.java
@@ -1,5 +1,21 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class PlanoEstudo {
+
+	private List<Edital> editais = new ArrayList<>();
+
+	private List<Cargo> cargos = new ArrayList<>();
+
+	private List<Disciplina> disciplinas = new ArrayList<>();
+
+	private List<Revisao> revisoes = new ArrayList<>();
+
+	private List<RegistroEstudo> constancia = new ArrayList<>();
+
+	private String observacao;
 
 }
---
+++ src/main/java/entities/RegistroEstudo.java
@@ -0,0 +1,17 @@
+package entities;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class RegistroEstudo {
+
+	private Date diaEstudado;
+	private Disciplina disciplina;
+	private Calendar tempoEstudo;
+	private String material;
+	private Integer numeroAcertoQuestao;
+	private Integer numeroErroQuesto;
+	private String paginas;
+	private String comentario;
+
+}
